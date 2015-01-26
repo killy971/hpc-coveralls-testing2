77fa80870f694c67d5c8e893a6c982f000831976
Guillaume Nargeot <killy971+github@gmail.com>
2015-01-26 21:02:08 +0900

    Use if-then-else instead of guards

--- src/Util.hs
+++ src/Util.hs
@@ -2,5 +2,6 @@ module Util where
 
 takeUntil :: (a -> Bool) -> [a] -> [a]
 takeUntil _ [] = []
-takeUntil p (x:xs) | p x = [x]
-                   | otherwise = x : takeUntil p xs
+takeUntil p (x:xs) = if p x
+    then [x]
+    else x : takeUntil p xs
